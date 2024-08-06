0535b1a6737043c826e053128b915912db32cf69
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2024-08-06 14:50:05 +0100

    Added methods to change endianness.

--- src/utils/Writer.java
+++ src/utils/Writer.java
@@ -3,6 +3,7 @@ package utils;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.Stack;
 
 public class Writer {
 
@@ -14,14 +15,26 @@ public class Writer {
         fileOutputStream.write(0);
         for(int symbol: symbolTable){
             byte[] bytes = ByteBuffer.allocate(4).putInt(symbol).array();
+            flipBytes(bytes);
             fileOutputStream.write(bytes);
         }
         fileOutputStream.write(0); //Signal symbol table end.
         for(int instruction: instructions){
             byte[] bytes = ByteBuffer.allocate(4).putInt(instruction).array();
+            flipBytes(bytes);
             fileOutputStream.write(bytes);
         }
         fileOutputStream.write(0); //Signal EOF.
         fileOutputStream.close();
+    }
+
+    private static void flipBytes(byte[] bytes){
+        Stack<Byte> byteStack = new Stack<>();
+        for(byte word: bytes){
+            byteStack.add(word);
+        }
+        for(int i = 0; i < bytes.length; ++i){
+            bytes[i] = byteStack.pop();
+        }
     }
 }
