dbfc8a88f5b3cf3c6daf820d4e6cdecce41530e0
Luvai Cutlerywala <64072072+LuvaiCutlerywala@users.noreply.github.com>
2024-08-07 13:03:24 +0100

    Simplified procedure and removed byte swapping.

--- src/assembler/BinaryGenerator.java
+++ src/assembler/BinaryGenerator.java
@@ -9,43 +9,30 @@ import java.util.Stack;
 
 public class BinaryGenerator {
 
-    private static final byte[] RESERVED = translateIntoBytes(0);
+    private static final int RESERVED = 0;
     private static ArrayList<Byte> output;
 
     public static byte[] generateBinary(SymbolTable symbolTable, Instruction[] instructions){
         output = new ArrayList<>();
         int[] translatedInstructions = CodeGenerator.translateInstructions(instructions);
         int[] translatedSymbolTable = SymbolTableEncoder.encodeSymbolTable(symbolTable);
-        addArray(RESERVED);
-        addArray(translateIntoBytes(translatedSymbolTable.length));
-        addArray(translateIntoBytes(translatedInstructions.length));
-        addArray(RESERVED);
+        add(RESERVED);
+        add(translatedSymbolTable.length);
+        add(translatedInstructions.length);
+        add(RESERVED);
         for(int symbol: translatedSymbolTable){
-            addArray(translateIntoBytes(symbol));
+            add(symbol);
         }
-        addArray(RESERVED);
+        add(RESERVED);
         for(int instruction: translatedInstructions){
-            addArray(translateIntoBytes(instruction));
+            add(instruction);
         }
         return unboxArray(output.toArray(new Byte[0]));
     }
 
-    private static byte[] translateIntoBytes(int target){
+    private static void add(int target){
         byte[] bytes = ByteBuffer.allocate(4).putInt(target).array(); //Java integers are 32 bit, i.e. 4 bytes.
-        Stack<Byte> byteStack = new Stack<>();
-        for(byte unit: bytes){
-            byteStack.add(unit);
-        }
-
-        for(int i = 0; i < bytes.length; ++i){
-            bytes[i] = byteStack.pop();
-        }
-
-        return bytes;
-    }
-
-    private static void addArray(byte[] array){
-        for(byte element: array){
+        for(byte element: bytes){
             output.add(element);
         }
     }
